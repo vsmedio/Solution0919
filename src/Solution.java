a264f3df2a6d2198244327eba13e60de3f2a583e
Arvis Abromovics <info@vsmedio.lv>
2018-01-08 20:35:40 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,29 @@
+/*
+Деление на ноль
+Создай метод public static void divisionByZero, в котором подели любое число на ноль и выведи на экран результат деления.
+Оберни вызов метода divisionByZero в try..catch. Выведи стек-трейс исключения используя метод exception.printStackTrace()
+
+
+Требования:
+1. В классе должен быть метод public static void divisionByZero().
+2. Метод divisionByZero должен содержать операцию деления любого числа на ноль.
+3. Метод divisionByZero должен вызывать System.out.println или System.out.print.
+4. Метод main должен содержать блок try..catch.
+5. Метод main должен отлавливать любые исключения метода divisionByZero.
+6. Программа должна выводить стек-трейс пойманого исключения.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try {
+            divisionByZero();
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void divisionByZero() {
+        System.out.println(5 / 0);
+    }
+}
